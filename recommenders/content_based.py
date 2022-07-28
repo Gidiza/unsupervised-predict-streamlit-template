6d7ea89d10bd561f8d5fb20452d4db7b9178df9f
Fabian <105725955+Fabian-B-explore@users.noreply.github.com>
2022-07-28 16:19:36 +0200

    tried my best to fix it

--- recommenders/content_based.py
+++ recommenders/content_based.py
@@ -39,7 +39,6 @@ movies = pd.read_csv('resources/data/movies.csv', sep = ',')
 ratings = pd.read_csv('resources/data/ratings.csv')
 movies.dropna(inplace=True)
 
-
 def data_preprocessing(subset_size):
     """Prepare data for use within Content filtering algorithm.
 
@@ -68,7 +67,8 @@ count_vec = CountVectorizer()
 count_matrix = count_vec.fit_transform(data['keyWords'])
 indices = pd.Series(data['title'])
 cosine_sim = cosine_similarity(count_matrix, count_matrix)
-    
+
+
 
 # !! DO NOT CHANGE THIS FUNCTION SIGNATURE !!
 # You are, however, encouraged to change its content.  
@@ -91,7 +91,12 @@ def content_model(movie_list,top_n=10):
     """
     # Initializing the empty list of recommended movies
     recommended_movies = []
-
+    data = data_preprocessing(27000)
+    # Instantiating and generating the count matrix
+    count_vec = CountVectorizer()
+    count_matrix = count_vec.fit_transform(data['keyWords'])
+    indices = pd.Series(data['title'])
+    cosine_sim = cosine_similarity(count_matrix, count_matrix)
     # Getting the index of the movie that matches the title
     idx_1 = indices[indices == movie_list[0]].index[0]
     idx_2 = indices[indices == movie_list[1]].index[0]
